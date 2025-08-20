db85d73a790968dcb4700518dba9937ee92e3618
varunvoora <2300030977@klunversity.in>
2025-08-20 10:59:32 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <Router basename="/S219_CICD">
+    <Router basename="/ecommerce_frontend">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
