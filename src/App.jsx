401b546aba4707e824a8167527895fe7133b6f21
Voora Bala Anjaneya Varun <163667771+varunvoora@users.noreply.github.com>
2025-08-20 15:25:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,38 +9,36 @@ import Computers from './components/Computers';
 import Mobiles from './components/Mobiles';
 import Laptops from './components/Laptops';
 import Pendrives from './components/Pendrives';
-import Home from './components/Home'
-import ProductPage from './components/ProductPage'
+import Home from './components/Home';
+import ProductPage from './components/ProductPage';
 import Cart from './components/Cart';
 import Payment from './components/Payment';
 import Orders from './components/Orders';
 
-
-
 function App() {
   const [count, setCount] = useState(0)
-  
+
   return (
     <>
-    <Router basename="/ecommerce_frontend">
-      <Routes>
-        <Route path="/" element={<HomePage />}>
-          <Route index element={<AboutUs />} />
-          <Route path="home" element={<Home />} />
-          <Route path="about-us" element={<AboutUs />} />
-          <Route path="computers" element={<Computers />} />
-          <Route path="mobiles" element={<Mobiles />} />
-          <Route path="laptops" element={<Laptops />} />
-          <Route path="pendrives" element={<Pendrives />} />
-          <Route path="login" element={<Login />} />
-          <Route path="signup" element={<Signup />} />
-          <Route path="productpage" element={<ProductPage />} />
-          <Route path="cart" element={<Cart />} />
-          <Route path="payment" element={<Payment />} />
-          <Route path="orders" element={<Orders />} />
-        </Route>
-      </Routes>
-    </Router>
+      <Router basename="/ecommerce">
+        <Routes>
+          <Route path="/" element={<HomePage />}>
+            <Route index element={<AboutUs />} />
+            <Route path="home" element={<Home />} />
+            <Route path="about-us" element={<AboutUs />} />
+            <Route path="computers" element={<Computers />} />
+            <Route path="mobiles" element={<Mobiles />} />
+            <Route path="laptops" element={<Laptops />} />
+            <Route path="pendrives" element={<Pendrives />} />
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+            <Route path="productpage" element={<ProductPage />} />
+            <Route path="cart" element={<Cart />} />
+            <Route path="payment" element={<Payment />} />
+            <Route path="orders" element={<Orders />} />
+          </Route>
+        </Routes>
+      </Router>
     </>
   )
 }
