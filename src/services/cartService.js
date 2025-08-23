18f6e6ba99c65b2bb14c079eb6235fa66e997a77
Voora Bala Anjaneya Varun <163667771+varunvoora@users.noreply.github.com>
2025-08-23 10:10:12 +0530

    Update cartService.js

--- src/services/cartService.js
+++ src/services/cartService.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/cart";
-
-const API_BASE = "http://localhost:8080/api/cart";
+const CART_API_URL = "http://localhost:9090/back1/api/cart";
 
 export const addToCart = async (userId, productId) => {
-  return await axios.post(`${API_BASE}/add`, {
+  return await axios.post(`${CART_API_URL}/add`, {
     userId,
     productId,
     quantity: 1
@@ -13,20 +11,19 @@ export const addToCart = async (userId, productId) => {
 };
 
 export const getCartItems = async (userId) => {
-  const response = await axios.get(`${API_BASE}/user/${userId}`);
+  const response = await axios.get(`${CART_API_URL}/user/${userId}`);
   return response.data;
 };
 
 export const clearCart = async (userId) => {
-  return await axios.delete(`${API_BASE}/clear/${userId}`);
+  return await axios.delete(`${CART_API_URL}/clear/${userId}`);
 };
 
 // Remove Cart Item
 export const removeCartItem = async (cartItemId) => {
     try {
-        await axios.delete(`${API_URL}/remove/${cartItemId}`);
+        await axios.delete(`${CART_API_URL}/remove/${cartItemId}`);
     } catch (error) {
         console.error("Error removing item from cart", error);
     }
 };
-
